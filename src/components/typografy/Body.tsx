a41ec7efad24f520ca70b7f4b61748781f71d1f6
Рустам <rustam@MacBook-Air-Rustam.local>
2024-11-04 01:57:58 +0300

    init next js

--- src/components/typografy/Body.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Body = styled.p`
-  font-size: ${props => props.theme.variables.fontSize};
-  line-height: ${props => props.theme.variables.grid};
-  margin: 0;
-`
